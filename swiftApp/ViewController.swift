e95639bf841ce2e967982205b8142f177730fd6f
rosi <rosica@shaw.ca>
2017-01-21 16:56:45 -0800

    my first commit

--- swiftApp/ViewController.swift
+++ swiftApp/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var tapCount = 0
+    
+
+    @IBAction func buttonTap(sender: AnyObject) {
+        tapCount = tapCount + 1
+        
+        if tapCount >= 2 {
+            self.view.backgroundColor = UIColor.grayColor()
+        }
+
+       }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
